5c3c55318b962e1e86023d9a14553d5c3a28bd77
David Pearce <david@differential.com>
2015-05-27 11:38:23 -0400

    Package update

---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  name: 'bevy:material-icons',
+  version: '1.0.62',
+  summary: 'A meteor package for the material design icon font.',
+  git: 'https://github.com/wearebevy/meteor-material-icons.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+
+  api.addFiles([
+    'MaterialDesign-Webfont/fonts/materialdesignicons-webfont.eot',
+    'MaterialDesign-Webfont/fonts/materialdesignicons-webfont.svg',
+    'MaterialDesign-Webfont/fonts/materialdesignicons-webfont.ttf',
+    'MaterialDesign-Webfont/fonts/materialdesignicons-webfont.woff',
+    'MaterialDesign-Webfont/fonts/materialdesignicons-webfont.woff2',
+    'MaterialDesign-Webfont/css/materialdesignicons.css'
+  ], 'client');
+
+});
