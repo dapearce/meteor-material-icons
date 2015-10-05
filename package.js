bd49e643c9f4827cca0c50715814726723230ec4
David Pearce <david@differential.com>
2015-10-05 11:22:41 -0400

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dapearce:material-icons',
   version: '0.1.3',
-  summary: 'A meteor package for the material design icon font.',
+  summary: 'A meteor package for the material design icons font.',
   git: 'https://github.com/dapearce/meteor-material-icons.git',
   documentation: 'README.md'
 });
