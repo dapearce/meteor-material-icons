4ca202618a6ce2cec510bbf46c5a269f287df7f7
David Pearce <david@differential.com>
2015-10-05 10:19:10 -0400

    Update new package api

--- package.js
+++ package.js
@@ -6,15 +6,18 @@ Package.describe({
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
+Package.onUse( function(api) {
 
   api.addFiles([
+    'MaterialDesign-Webfont/css/materialdesignicons.css'
+  ], 'client');
+
+  api.addAssets([
     'MaterialDesign-Webfont/fonts/materialdesignicons-webfont.eot',
     'MaterialDesign-Webfont/fonts/materialdesignicons-webfont.svg',
     'MaterialDesign-Webfont/fonts/materialdesignicons-webfont.ttf',
     'MaterialDesign-Webfont/fonts/materialdesignicons-webfont.woff',
-    'MaterialDesign-Webfont/fonts/materialdesignicons-webfont.woff2',
-    'MaterialDesign-Webfont/css/materialdesignicons.css'
+    'MaterialDesign-Webfont/fonts/materialdesignicons-webfont.woff2'
   ], 'client');
 
 });
